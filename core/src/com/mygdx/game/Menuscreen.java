f43a395b7d35f7813ee429fc791ceee0775e6250
Stefan <lucis0783@googleapps.wrdsb.ca>
2015-12-11 10:04:46 -0500

    Created new font styles

--- core/src/com/mygdx/game/Menuscreen.java
+++ core/src/com/mygdx/game/Menuscreen.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Color;
@@ -21,19 +22,20 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class Menuscreen extends ApplicationAdapter {
 	private static final float BUTTON_WIDTH = 300f;
-	private static final float BUTTON_HEIGHT = 60f;
+	private static final float BUTTON_HEIGHT = 30f;
 	SpriteBatch batch;
 	Texture tBack, tButton;
-	TextureRegion region;
 	Stage stage;
 	TextureAtlas taButton;
 	BitmapFont fWhite, fBlack;
 	TextButton tbStart, tbExit;
-	TextButton.TextButtonStyle tbsNewGame, textButtonStyle;
+	TextButton.TextButtonStyle textButtonStyle;
 	Table table;
 	Skin skNewGame;
 	Label laTitle;
-	int nSHeight, nSWidth, nChoi=0;
+	LabelStyle headingstyle;
+	Music music;
+	int nChoi=0;
 	
 	@Override
 	public void create () {
@@ -41,14 +43,22 @@ public class Menuscreen extends ApplicationAdapter {
 		stage= new Stage();
 		Gdx.input.setInputProcessor(stage);
 
+		//creates background music
+		//http://www.norakomi.com/tutorial_mambow2_music.php
+		music = Gdx.audio.newMusic(Gdx.files.internal("Music/Halo- Menu Music.mp3"));
+		music.setLooping(true);
+		music.setVolume(music.getVolume() * 1 / 5);
+		music.play();
+
 		//used Bitmap Font Generator to make different fonts
+		//http://www.angelcode.com/products/bmfont/
 		fWhite = new BitmapFont(Gdx.files.internal("Fonts/white.fnt"));
 		fBlack = new BitmapFont(Gdx.files.internal("Fonts/black.fnt"));
 
 		tBack = new Texture(Gdx.files.internal("Picnic.jpg"));
-		region = new TextureRegion(tBack, 20, 20, 50, 50);
 
 		//menu button and pack comes from TheDeepDarkTaurock code
+		//creates buttons
 		taButton = new TextureAtlas("MenuButton.pack");
 		skNewGame = new Skin(taButton);
 		tButton = new Texture(Gdx.files.internal("expences-button-png-hi.png"));
@@ -59,15 +69,14 @@ public class Menuscreen extends ApplicationAdapter {
 		textButtonStyle.over = skNewGame.newDrawable("MenuButtonUp");
 		textButtonStyle.font = fWhite;
 
-		LabelStyle headingstyle = new LabelStyle(fWhite, Color.WHITE);
-		headingstyle.font = fWhite;
+		//creates title
+		headingstyle = new LabelStyle(fBlack, Color.WHITE);
+		laTitle = new Label("Ants 'n Stuff", headingstyle);
+		laTitle.setFontScale(2);
 
-		laTitle = new Label("Ants", headingstyle);
-		laTitle.setSize(600f, 50f);
-
-
+		//creates start game button
 		tbStart = new TextButton("Start", textButtonStyle);
-		tbStart.pad(20f);
+		tbStart.pad(10f);
 		tbStart.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
 		tbStart.addListener(new ClickListener() {
 			@Override
@@ -77,8 +86,9 @@ public class Menuscreen extends ApplicationAdapter {
 			}
 		});
 
+		//creates exit game button
 		tbExit = new TextButton("Exit", textButtonStyle);
-		tbExit.pad(20f);
+		tbExit.pad(10f);
 		tbExit.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
 		tbExit.addListener(new ClickListener() {
 			@Override
@@ -86,15 +96,16 @@ public class Menuscreen extends ApplicationAdapter {
 				Gdx.app.exit();
 			}
 		});
+
+		//put everything into a table and onto the stage
 		table = new Table();
-		table.setBounds(0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		table.setFillParent(true);
 		table.add(laTitle);
 		table.row();
 		table.add(tbStart);
 		table.row();
 		table.add(tbExit);
-		table.setDebug(true);
 		stage.addActor(table);
 	}
 
@@ -102,10 +113,9 @@ public class Menuscreen extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		/*batch.begin();
-		//batch.draw(region, 10, 10);
-		System.out.print("hi");
-		batch.end();*/
+		stage.getBatch().begin();
+		stage.getBatch().draw(tBack, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		stage.getBatch().end();
 		stage.act();
 		stage.draw();
 	}
